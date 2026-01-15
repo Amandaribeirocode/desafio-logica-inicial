ada219b40470a448ffc42044d2517939ce51ce0c
Amanda Ribeiro <karolyneamanda81@gmail.com>
2026-01-15 14:45:46 -0300

    Desafio 3

---
+++ desafio3.js
@@ -0,0 +1,35 @@
+
+
+class heroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+    let ataque; 
+    if(this.tipo === "mago"){
+        ataque = "magia";
+    }
+    else if(this.tipo === "guerreiro"){
+        ataque = "espada";
+    }
+    else if(this.tipo === "monge"){
+        ataque = "artes marciais";
+    }
+    else if(this.tipo === "ninja"){
+        ataque = "shuriken";
+    }
+    else {
+        ataque = "ataque desconhecido";
+    }
+
+       console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+
+}
+
+let heroi1 = new heroi("Amanda", 25, "mago");
+
+heroi1.atacar();
